689764d5bb2785efd52fd9879b6e42251edfee0b
Sowmya Ramesh <soumyaramesh2002@gmail.com>
2023-12-24 21:51:54 +0530

    updates in product api

--- api/service-products/product.py
+++ api/service-products/product.py
@@ -4,10 +4,12 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy as db 
 from sqlalchemy import Column,Integer,String
-import jwt
-import requests
 from flask import jsonify
-import json
+from flask_cors import CORS
+import uuid
+
+app = Flask(__name__)
+CORS(app,origins='http://localhost:5173')
 
 
 engine=db.create_engine("sqlite:///products.sqlite")
@@ -32,7 +34,7 @@ metadata.create_all(engine)
 Base=declarative_base()
 session=sessionmaker(bind=engine)()
 
-app=Flask(__name__)
+
 
 class Products(Base):
     __tablename__="products"
@@ -68,13 +70,8 @@ class Products(Base):
             "category":self.category,
             "discount":self.discount,
             "gender":self.gender
-
         })
     
-    # def validate(self):
-    #     val_account_no=len(str(self.account_no))==10 
-    #     val_password=len(str(self.password))>6
-    #     return val_account_no and val_password
     
 class BaseException(Exception):
     status=400
@@ -93,95 +90,34 @@ class ProductnotfoundError(BaseException):
     def __init__(self) ->None:
         super().__init__(404,"Invalid product or product not found")
 
-# class BankholderNotPresent(BaseException):
-#     def __init__(self) -> None:
-#         super().__init__(404,"bankholder not present")
-
-# class WithdrwalError(BaseException):
-#     def __init__(self) -> None:
-#         super().__init__(404,"Can't withdrawl money greater then deposit ammount")
-
-# class DepositAmountError(BaseException):
-#     def __init__(self) -> None:
-#         super().__init__(404,"amount sholud not be negative")
-
-# class AuthenticationError(BaseException):
-#     def __init__(self) -> None:
-#         super().__init__(404,"invalid password or username")
-
-
-# class TokenGenerationError(BaseException):
-#     def __init__(self) ->None:
-#         super().init__(500,"unable to generate the token")    
 
 @app.route("/api/products",methods=["POST"])
 def create_product():
-    newproduct=request.json
-    productobj=Products(newproduct['id'],newproduct['name'],newproduct['description'],newproduct['image'],
-                              newproduct['price'],newproduct['gender'],newproduct['category'],newproduct['discount'])
-
-    # if not banks.validate():
-    #     err=ValidationError()
-    #     return str(err),err.status
-
+    newproduct=request.form
+    productobj=Products(str(uuid.uuid4()),newproduct['name'],newproduct['description'],"empty",newproduct['price'],newproduct['category'],newproduct['discount'],newproduct['gender'])
     session.add(productobj)
     session.commit()
     return make_response(""),200
 
     
 
-@app.route("/api/products",methods=["GET"])
+@app.route("/api/products", methods=["GET"])
 def show_products():
-    allproducts=session.query(products).all()
-    return str(allproducts),200
-
-    # product_details=session.query(products).Get(account_no=details['account_no']).first()
-    # if not (account_details):
-    #     err=BankholderNotPresent()
-    #     return str(err),err.status
-    # else:
-    #     user=account_details
-    # # if (details['name']!=user.name) and (details['password']!=user.password):
-    # if (details['name']!=payload["user_name"]) and (details['password']!=payload["password"]):
-    
-    #     err=AuthenticationError()
-    #     return str(err),err.status
-    # if  details['amount']<=0:
-    #     err=DepositAmountError()
-    #     return str(err),err.status
-    # accno=details['account_no']
-    # acc=session.query(bankholders).filter_by(account_no=details['account_no']).first()
-    # acc.balance=acc.balance +details['amount']
-    
-    # session.commit()
-
-    
-
-# @app.route("/api/bankholder/withdraw/",methods=["patch"])
-
-# def withdrawl():
-#     money=request.json
-#     account_details=session.query(bankholders).filter_by(account_no=money['account_no']).first()
-
-#     if not (account_details):
-#         err=BankholderNotPresent()
-#         return str(err),err.status
-#     else:
-#         curent=account_details
-#     if (money['name']!=curent.name) and (money['password']!=curent.password):
-#         err=AuthenticationError()
-#         return str(err),err.status
-    
-#     if money['amount']>=curent.balance:
-#         err=WithdrwalError()
-#         return str(err),err.status
-#     accno=money['account_no']
-#     acc=session.query(bankholders).filter_by(account_no=money['account_no']).first()
-#     acc.balance=acc.balance - money['amount']
-#     session.commit()
-    
-
-#     return str(acc),200
+    all_products = session.query(Products).all()
+    products_list = [
+        {
+            "name": product.name,
+            "id": product.id,
+            "description": product.description,
+            "image": product.image,
+            "price": product.price,
+            "category": product.category,
+            "discount": product.discount,
+            "gender": product.gender
+        }
+        for product in all_products
+    ]
+    return jsonify(products_list), 200
 
 @app.route("/api/products/<string:id>", methods=["DELETE"])
 def delete_product(id):
@@ -193,12 +129,13 @@ def delete_product(id):
         session.delete(product)
         session.commit()
         return make_response("", 200)
+from flask import jsonify
 
 @app.route("/api/products/<id>", methods=["GET"])
 def show_detail(id):
     product = session.query(Products).filter_by(id=id).first()
     if not product:
-        err = ProductnotfoundError()
+        err = ProductNotFoundError()
         return str(err), err.status
     else:
         # Convert the product object to a dictionary
@@ -230,7 +167,7 @@ def edit_products(id):
     session.commit() 
     return make_response("", 200)
     
-   
+app.run(debug=True)  
 
    
 
